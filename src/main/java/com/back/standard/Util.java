cbb57ca2d4d462b80161d9dde9f2f96795934c3b
chacha86 <taejincha0619@gmail.com>
2026-07-27 11:51:41 +0900

    findAll

--- src/main/java/com/back/standard/Util.java
+++ src/main/java/com/back/standard/Util.java
@@ -5,6 +5,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Util {
 
@@ -107,6 +108,16 @@ public class Util {
                 Files.createDirectories(getPath(dirPath));
             } catch (IOException e) {
                 throw new RuntimeException("디렉토리 생성 실패: " + dirPath, e);
+            }
+        }
+
+        public static Stream<Path> walkRegularFiles(String dirPath, String fileNameRegex) {
+            try {
+                return Files.walk(Path.of(dirPath))
+                        .filter(Files::isRegularFile)
+                        .filter(path -> path.getFileName().toString().matches(fileNameRegex));
+            } catch (IOException e) {
+                return Stream.empty();
             }
         }
     }
